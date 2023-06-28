47f73bb0a078da64b69de3a062ae510e1eb5e691
Yuki Arami <wkgtyou0922@gmail.com>
2023-06-28 23:40:53 +0900

    遷移先のUI作成

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
 class NextPage extends StatelessWidget {
-  const NextPage({Key ? key}) : super(key : key);
+  const NextPage({Key ? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: const Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                    width: 30,
+                    child: Text('ID')),
+                Text(':　毎回同じIDを表示'),
+              ],
+            ),
+            SizedBox(height: 30,),
+            Row(
+              children: [
+                SizedBox(
+                    width: 30,
+                    child: Text('PW')),
+                Text(':　毎回同じPWを表示'),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
